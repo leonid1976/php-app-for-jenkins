47c26ad6e7300ee76e1c97702576cae84cd58dbb
leonid1976 <leonid.neigel@gmail.com>
2022-03-21 08:57:37 +1000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
   $file = '/tmp/sample-app.log';
   $message = file_get_contents('php://input');
-  file_put_contents($file, date('Y-m-d H:i:s') . " Received message!: " . $message . "\n", FILE_APPEND);
+  file_put_contents($file, date('Y-m-d H:i:s') . " Received message!!: " . $message . "\n", FILE_APPEND);
 }
 else
 {
